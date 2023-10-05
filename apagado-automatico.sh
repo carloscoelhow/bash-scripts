266da076cb797c7fe3928223f3a0a86dfb2c2d26
Carlos Coelho Jr <carloscoelhow@users.noreply.github.com>
2023-10-05 11:16:32 +0200

    Update apagado-automatico.sh

--- apagado-automatico.sh
+++ apagado-automatico.sh
@@ -8,9 +8,9 @@
 #pero se puede cambiarlo para cuantas horas se quiera
 sudo printf '%s\n' '#!/bin/bash' 'sleep 7200' 'sudo shutdown now' > /usr/bin/apagado.sh
 sudo chmod +x /usr/bin/apagado.sh
-sudo printf '%s\n' '[Unit]' 'Description=Scrip para apagado automatico de la instancia después de una hora' '' '[Service]' 'ExecStart=/usr/bin/apagado.sh' '' '[Install]' 'WantedBy=multi-user.target' > /lib/systemd/system/apagado_automatico.service
+sudo printf '%s\n' '[Unit]' 'Description=Scrip para apagado automatico de la instancia después de una hora' '' '[Service]' 'ExecStart=/usr/bin/apagado.sh' '' '[Install]' 'WantedBy=multi-user.target' > /lib/systemd/system/apagado-automatico.service
 sudo systemctl daemon-reload
-sudo systemctl enable apagado_automatico.service
-sudo systemctl start apagado_automatico.service
-sudo systemctl status apagado_automatico.service
+sudo systemctl enable apagado-automatico.service
+sudo systemctl start apagado-automatico.service
+sudo systemctl status apagado-automatico.service
 echo "apagado automático creado"
